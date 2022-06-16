88e86832199f3fb5dad1f84bc1e718aa4ce26d7f
josephtannoury <josephtannoury@outlook.com>
2022-06-17 01:31:48 +0300

    feature: setting up db tables

--- backend/database/migrations/2014_10_12_000000_create_questions_table.php
+++ backend/database/migrations/2014_10_12_000000_create_questions_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('content')->unique();
+            $table->integer('survey_id')->unique();
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('users');
     }
 };
