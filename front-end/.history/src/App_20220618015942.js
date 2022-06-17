06f6f526c19c92fec651d29307e2ddbbdf4b11fe
josephtannoury <josephtannoury@outlook.com>
2022-06-18 02:00:06 +0300

    feature: adding Radio component | Reactjs

--- front-end/.history/src/App_20220618015942.js
+++ front-end/.history/src/App_20220618015942.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import Admin  from "./components/Admin";
-import Radio from "./components/Radio";
+// import Radio from "./components/Radio";
 import Checkbox from "./components/Checkbox";
 // import Drop from "./components/Drop";
 
@@ -13,7 +13,7 @@ function App() {
         </div>
         <div className="box">
           <h1 className="box-title">Lorem Ipsum dolor sit amend</h1>
-          <Radio/>
+          <R/>
         </div>
         
     </div>
--- front-end/.history/src/App_20220618015944.js
+++ front-end/.history/src/App_20220618015944.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import Admin  from "./components/Admin";
-import Radio from "./components/Radio";
+// import Radio from "./components/Radio";
 import Checkbox from "./components/Checkbox";
 // import Drop from "./components/Drop";
 
