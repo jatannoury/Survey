c917e83dc32bfdc60ed5ba014dfe2c9b1656b147
josephtannoury <josephtannoury@outlook.com>
2022-06-18 01:57:08 +0300

    feature: adding checkbox component | Reactjs

--- front-end/.history/src/App_20220618015631.js
+++ front-end/.history/src/App_20220618015631.js
@@ -13,7 +13,7 @@ function App() {
         </div>
         <div className="box">
           <h1 className="box-title">Lorem Ipsum dolor sit amend</h1>
-          
+            
         </div>
         
     </div>
