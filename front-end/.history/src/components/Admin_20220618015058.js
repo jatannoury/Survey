563b0ae306faa7f1f8b2cdfeccccf54228ab97cb
josephtannoury <josephtannoury@outlook.com>
2022-06-18 01:51:35 +0300

    feature: adding password  login container | Reactjs

--- front-end/.history/src/components/Admin_20220618015058.js
+++ front-end/.history/src/components/Admin_20220618015058.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "../App.css";
 import Username from "../components/Username";
-import Password from "../components/Password";
+// import Password from "../components/Password";
 
 const Admin = () => {
   return (
